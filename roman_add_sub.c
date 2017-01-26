3e2c37065339da57ae914fe1e04f9975ebc4934a
bhargav0592 <bhargavs.gade@gmail.com>
2017-01-25 20:02:52 -0500

    validate input option (add/substract) only

--- roman_add_sub.c
+++ roman_add_sub.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void usage() {
 	printf("./roman_add_sub <opt> <str1> <str2>\n");
@@ -10,12 +11,26 @@ void usage() {
 }
 
 
-int main(int argc, char *agrv[]) {
+int main(int argc, char *argv[]) {
 	if (argc != 4) {
 		printf("Please follow the instructions:\n");
 		usage();
 		return -1;
 	}
+
+	char *opt, *str1, *str2;
+
+	opt = argv[1];
+	str1 = argv[2];
+	str2 = argv[3];
+	(void) str1;
+	(void) str2;
+	if (strlen(opt) != 1 || (opt[0] != '1' && opt[0] != '2')) {
+		printf("Invalid option\n");
+		usage();
+		return -1;
+	}
+
 
 return 0;
 }
