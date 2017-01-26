c31df2ab17e6729bf16b8a750f84ac13a1edcf89
bhargav0592 <bhargavs.gade@gmail.com>
2017-01-26 00:09:02 -0500

    validate input string (should contain only roman numbers)

--- roman_add_sub.c
+++ roman_add_sub.c
@@ -10,6 +10,27 @@ void usage() {
 	return ;
 }
 
+/*
+ * String Should contain only I,V,X,L,C,D,M characters.
+*/
+int validate_input_string(char *temp) {
+	char *input;
+	int len;
+
+	input = temp;
+	len = strlen(input);
+	if (len == 0)
+		return -1;
+	for (int i = 0; i < len; i++) {
+		if (input[i] != 'I' && input[i] != 'V'
+			&& input[i] != 'X' && input[i] != 'L'
+			&& input[i] != 'C' && input[i] != 'D'
+			&& input[i] != 'M') {
+			return -1;
+		}
+	}
+	return 0;
+}
 
 int main(int argc, char *argv[]) {
 	if (argc != 4) {
@@ -19,6 +40,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	char *opt, *str1, *str2;
+	int ret;
 
 	opt = argv[1];
 	str1 = argv[2];
@@ -31,6 +53,41 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	if (str1)
+		ret = validate_input_string(str1);
+	else {
+		printf("Input String1 is NULL\n");
+		return -1;
+	}
+
+	if (ret) {
+		printf("Input (%s) should contain only roman numbers\n", str1);
+		return ret;
+	}
+
+	if (str2)
+		ret = validate_input_string(str2);
+	else {
+		printf("Input String2 is NULL\n");
+		return -1;
+	}
+
+	if (ret) {
+		printf("Input (%s) should contain only roman numbers\n", str2);
+		return ret;
+	}
+
+	str1 = strdup(str1);
+	str2 = strdup(str2);
+
+	printf("Input 1 : %s\nInput 2 : %s\n", str1, str2);
+	goto out;
+
+out:
+	if (str1)
+		free(str1);
+	if (str2)
+		free(str2);
 
 return 0;
 }
